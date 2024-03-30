f20b1e507fbc504f162be2e5af1374774bc586a8
sagefantasma <sagefantasma@gmail.com>
2024-03-30 19:01:15 -0400

    added logistics of first pass at multithreaded scanning.

--- Simplified Memory Manager/ScanThread.cs
+++ Simplified Memory Manager/ScanThread.cs
@@ -1,23 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Simplified_Memory_Manager
 {
-    internal class ScanThread
-    {
-        public byte[] Data { get; set; }
-        public CancellationToken Token { get; set; }
-        public SimplePattern Pattern { get; set; }
+	public delegate void PatternMatchedEventHandler(object sender, int index);
 
-        public void ScanForPattern()
-        {
-            for(int index = 0; index < Data.Length; index++)
-            {
-                // if (Data[index])
-            }
-        }
-    }
+	internal class ScanThread
+	{
+		public byte[] Data { get; set; }
+		public CancellationToken Token { get; private set; }
+		public SimplePattern Pattern { get; private set; }
+		PatternMatchedEventHandler PatternMatched {get;set;}
+		
+		public ScanThread(SimplePattern pattern, CancellationToken token, PatternMatchedEventHandler patternMatched)
+		{
+			Pattern = pattern;
+			Token = token;
+			PatternMatched = patternMatched;
+			PatternWasMatched += patternMatched;
+		}
+		
+		public event PatternMatchedEventHandler PatternWasMatched
+		{
+			add
+			{
+				PatternWasMatched += value;
+			}
+			remove
+			{
+				PatternWasMatched += value;
+			}
+		}
+
+		public void ScanForPattern(ref int foundPosition)
+		{
+			for(int dataIndex = 0; dataIndex < Data.Length; dataIndex++)
+			{
+				for(int patternIndex = 0; patternIndex < Pattern.ParsedPattern.Count; patternIndex++)
+				{
+					PatternExpression currentExpression = Pattern.ParsedPattern[patternIndex];
+					if(currentExpression.Operation == Operation.SkipOne)
+					{
+						continue;
+					}
+					else if(currentExpression.Operation == Operation.Exact)
+					{
+						if(currentExpression.Operand != Data[dataIndex + patternIndex])
+						{
+							//not a match, this slice is no good
+							break;
+						}
+						else if(patternIndex == Pattern.ParsedPattern.Count - 1 )
+						{
+							//perfect match
+							foundPosition = dataIndex;
+							PatternMatched(this, dataIndex);
+						}
+						else
+						{
+							//looking good so far, but not a perfect match yet
+							continue;
+						}
+					}
+				}
+			}
+		}
+	}
 }
