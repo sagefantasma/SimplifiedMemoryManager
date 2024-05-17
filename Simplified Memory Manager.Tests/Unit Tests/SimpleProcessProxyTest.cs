5f9bde072f709405729b8d8255651d0d76f88f5b
sagefantasma <sagefantasma@gmail.com>
2024-05-16 22:52:44 -0400

    separating integration and unit tests

---
+++ Simplified Memory Manager.Tests/Unit Tests/SimpleProcessProxyTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+
+namespace SimplifiedMemoryManager.Tests;
+
+public class SimpleProcessProxyTest
+{
+    [Fact]
+    public void CanCreateProcessProxy()
+    {
+        //Arrange
+        SimpleProcessProxy simpleProcessProxy;
+        Process process = Process.GetCurrentProcess();
+
+        //Act
+        simpleProcessProxy = new SimpleProcessProxy(process);
+
+        //Assert
+        Assert.NotNull(simpleProcessProxy);
+    }
+
+    [Fact]
+    public void ThrowsExceptionIfNullProcess()
+    {
+        //Arrange
+        SimpleProcessProxy simpleProcessProxy;
+
+        //Act
+        void act()
+        {
+            simpleProcessProxy = new SimpleProcessProxy(null);
+        }
+
+        //Assert
+        Assert.Throws<SimpleProcessProxyException>(act);
+    }
+
+    [Fact]
+    public void CanGetProcessSnapshot()
+    {
+        //Arrange
+        using (SimpleProcessProxy simpleProcessProxy = new SimpleProcessProxy(Process.GetCurrentProcess()))
+        {
+            byte[] data;
+
+            //Act
+            data = simpleProcessProxy.GetProcessSnapshot();
+
+            //Assert
+            Assert.NotNull(data);
+        }
+    }
+
+    [Fact]
+    public void TryingToReadInvalidValuesThrowsExpectedException()
+    {
+        //Arrange
+        using (SimpleProcessProxy simpleProcessProxy = new SimpleProcessProxy(Process.GetCurrentProcess()))
+        {
+            //Act
+            void impossibleRead()
+            {
+                byte[] result = simpleProcessProxy.ReadProcessOffset(-10, 10);
+            }
+
+            //Assert
+            Assert.Throws<SimpleProcessProxyException>(impossibleRead);
+        }
+    }
+}
